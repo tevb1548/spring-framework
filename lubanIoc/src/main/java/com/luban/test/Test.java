59bd498725e9491a15b2fffdd157ee99220d6c19
1789050006@qq.com <1789050006@qq.com>
2019-05-28 13:25:50 +0800

    2019-05-28

--- lubanIoc/src/main/java/com/luban/test/Test.java
+++ lubanIoc/src/main/java/com/luban/test/Test.java
@@ -9,6 +9,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * @date 2019/5/20 - 15:00
  */
 public class Test {
+	/**
+	 * 普通的注册是在扫描之后完成注册
+	 * 解析configBean->扫描得到的所有@Conponent的bd->处理得到的所有@import(
+	 * 1.importSelector 识别完后先configurationClasses.put();然后再注册
+	 * 2.registrar 识别完后先importBeanDefinitionRegistrars.put();然后再注册
+	 * 3.import普通类 识别完后先configurationClasses.put();然后判断之前是否注册
+	 * )
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		/**
 		 * 1.创建工厂对象
